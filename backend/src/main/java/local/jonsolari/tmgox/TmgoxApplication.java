5196fc39331e1554f11acde6191aed944af410f4
System Administrator <jon.s.solari@gmail.com>
2020-06-12 14:20:44 -0400

    java backend started

---
+++ backend/src/main/java/local/jonsolari/tmgox/TmgoxApplication.java
@@ -0,0 +1,14 @@
+package local.jonsolari.tmgox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmgoxApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TmgoxApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/local/jonsolari/tmgox/TmgoxApplicationTests.java
@@ -0,0 +1,15 @@
+package local.jonsolari.tmgox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmgoxApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
