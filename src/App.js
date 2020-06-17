8c5f032f09120715d210854fc0ff53948ff4b353
System Administrator <jon.s.solari@gmail.com>
2020-06-17 15:12:17 -0400

    added wishlist component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import EPs from './components/EPs';
 import About from './components/About';
 import Register from './components/Register';
 import Login from './components/Login';
+import Wishlist from './components/Wishlist';
+import PrivateRoute from './components/PrivateRoute';
 
 import './App.css';
 
@@ -19,15 +21,18 @@ function App(){
         <div class="main">
           <Router>
             <div class="tab">
-              <h1>TMGOX the Mountain Goats Online Exchange</h1>
               <Link to="/"><img class="logo" alt="tMGOX logo" src="/img/tMGox.png"/></Link>
               <Link to="/" class="lynx">THE FIRST FIVE</Link>
-              <Link to="/extendedplays" class="lynx">EPs</Link>
+              <Link to="/extendedplays" class="lynx">EPs &c.</Link>
               <Link to="/goodluckchief" class="lynx">GOOD LUCK CHIEF</Link>
               <Link to="/about" class="lynx">ABOUT</Link>
               <p class="lynx"><Link to="/register" class="white">REGISTER</Link>/<Link to="/login" class="white">LOGIN</Link></p>
             </div>
-       
+            <div class="rightside">
+            <div class="header">
+              <h2 class="titletext">the Mountain Goats Online eXchange</h2>
+            <p>a place to track prices of your favorite records by John Darnielle et al (or by other bands too)</p>
+            </div>
           
               <Route exact path="/" component={First5} />
               <Route exact path="/extendedplays" component={EPs} />
@@ -35,9 +40,11 @@ function App(){
               <Route exact path="/about" component={About} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              
-          
+              <PrivateRoute exact path="/wishlist" component={Wishlist} />
+              </div>
           </Router>   
+          
+          
         </div>
   )
 }
--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,8 @@ export default function About(){
     return(
         <div class="About">
             <p>Hey, how's it going.</p>
-            <p>This website started as a simple React.js project hoping to showcase the ridiculousness of the rare-indie-record resale market. It is also a dumb Bitcoin joke.</p>
+            <p>This website started as a simple React.js project hoping to showcase the ridiculousness of the rare-indie-record resale market.</p>
+            <p>It is also a dumb Bitcoin joke.</p>
             <p>If you want to hire me, or send me a copy of any of the records listed on this site, email me at (jon.s.solari at gmail).</p>
         </div>
     )
