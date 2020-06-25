c71dca34c88e1aa6d0ff407a420c590e6391640d
System Administrator <jon.s.solari@gmail.com>
2020-06-25 17:47:55 -0400

    progress on comparison machine

--- scrapecompare.py
+++ scrapecompare.py
@@ -22,7 +22,7 @@ def get_detail_data(soup):
     total = []
 
     if nope != []:
-        return 'Nope'
+        return None
     else:
         # total.append(h3)
         total.append(span[1:])
@@ -33,23 +33,23 @@ def get_detail_data(soup):
 # TODO loop over list of releases to search for, update JSON / remove things no longer available 
 
 # make sure account for plain black pressings of BPT and MCB
-releaselist = [(1, "mountain goats zopilote machine lp"), (2, "mountain goats sweden lp"),  (3, "mountain goats nothing for juice lp"), (4, "mountain goats full force galesburg lp"),  (5, "mountain goats coroner's gambit lp"), (6, "mountain goats new asian cinema"),  (7, "mountain goats isopanisad radio hour"), (8, "mountain goats devil in the shortwave"), (9,"mountain goats black pear tree"), (10, "mountain goats satanic messiah"), (11, "mountain goats moon colony bloodbath"), (12, "mountain goats taboo cassette"), (13, "mountain goats come come to the sunset tree"), (14, "mountain goats on juhu beach"), (15, "mountain goats black pear tree yellow"),  (16, "mountain goats moon colony bloodbath green"), (17, "beatles please please me parlophone pmc 1202 first")]
-ebaydata = {}
+releaselist = ["mountain goats zopilote machine lp", "mountain goats sweden lp", "mountain goats nothing for juice lp", "mountain goats full force galesburg lp", "mountain goats coroner's gambit lp", "mountain goats new asian cinema", "mountain goats isopanisad radio hour", "mountain goats devil in the shortwave", "mountain goats black pear tree", "mountain goats satanic messiah", "mountain goats moon colony bloodbath", "mountain goats taboo cassette", "mountain goats come come to the sunset tree", "mountain goats on juhu beach", "mountain goats black pear tree yellow", "mountain goats moon colony bloodbath green", "beatles please please me parlophone pmc 1202 first"]
+
+discogsurls = ['https://api.discogs.com/releases/1601354', 'https://api.discogs.com/releases/1191772', 'https://api.discogs.com/releases/744810', 'https://api.discogs.com/releases/523351', 'https://api.discogs.com/releases/1217804', 'https://api.discogs.com/releases/1625208', 'https://api.discogs.com/releases/1625203', 'https://api.discogs.com/releases/1210894', 'https://api.discogs.com/releases/1526609', 'https://api.discogs.com/releases/1516102','https://api.discogs.com/releases/1763341', 'https://api.discogs.com/releases/2767923', 'https://api.discogs.com/releases/769960', 'https://api.discogs.com/releases/2873969', 'https://api.discogs.com/releases/4880289', 'https://api.discogs.com/releases/5118937', 'https://api.discogs.com/releases/1350468']
+
+pricedata = {}
 
 
-def getdata(release):
+def ebayget(release):
     search = release.replace(" ", "+")
     url = 'https://www.ebay.com/sch/176985/i.html?_from=R40&_nkw={}&LH_TitleDesc=0&_sop=15'.format(search)
     return get_detail_data(get_page(url))
 
+def discogsget(release):
+    r = requests.get(release).json()
+    return r['lowest_price']
+
 for i in releaselist:
-    ebaydata[i[0]] = getdata(i[1])
+    pricedata[i] = ebayget(i)
 
-
-
-
-
-
-discogsurls = [(1, 'https://api.discogs.com/releases/1601354'), (2, 'https://api.discogs.com/releases/1191772'), (3, 'https://api.discogs.com/releases/744810'), (4, 'https://api.discogs.com/releases/523351'), (5, 'https://api.discogs.com/releases/1217804'), (6, 'https://api.discogs.com/releases/1625208'), (7, 'https://api.discogs.com/releases/1625203'), (8, 'https://api.discogs.com/releases/1210894'), (9, 'https://api.discogs.com/releases/1526609'), (10, 'https://api.discogs.com/releases/1516102'), (11, 'https://api.discogs.com/releases/1763341'), (12, 'https://api.discogs.com/releases/2767923'), (13, 'https://api.discogs.com/releases/769960'), (14, 'https://api.discogs.com/releases/2873969'), (15, 'https://api.discogs.com/releases/4880289'), (16, 'https://api.discogs.com/releases/5118937'), (17, 'https://api.discogs.com/releases/1350468')]
-
-print(ebaydata)
+print(pricedata)
