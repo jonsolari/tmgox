f5dd515eebc849bb822e1546288bee875b8907ef
Jon Solari <jon.s.solari@gmail.com>
2023-08-03 13:26:58 -0400

    scraping method changed to match ebay's new html semantics

--- scrapecompare.py
+++ scrapecompare.py
@@ -1,5 +1,7 @@
 import sys
 import requests
+import json
+import time
 from bs4 import BeautifulSoup
 
 
@@ -13,11 +15,11 @@ def get_page(url):
     return soup
 
 def get_detail_data(soup):
-    nope = soup.findAll(text='No exact matches found')
+    nope = soup.findAll(string='No exact matches found')
     
-    h3 = soup.find('h3', class_="s-item__title").text
-    span = soup.find('span', class_="s-item__price").text
-    href = soup.find('a', class_="s-item__link", href=True).get('href')
+    h3 = soup.find('div', {'id': 'srp-river-results'}).find('div', class_="s-item__title").find('span').text
+    span = soup.find('div', {'id': 'srp-river-results'}).find('span', class_="s-item__price").text
+    href = soup.find('div', {'id': 'srp-river-results'}).find(class_="s-item__image").find('a', href=True).get('href')
 
     total = { 'price': 0, 'url' : '' }
 
@@ -28,13 +30,9 @@ def get_detail_data(soup):
         total['url'] = href
         return total
 
+releaselist = ["mountain goats zopilote machine lp", "mountain goats sweden lp", "mountain goats nothing for juice lp", "mountain goats full force galesburg lp", "mountain goats coroner's gambit lp", "mountain goats new asian cinema", "mountain goats isopanisad radio hour", "mountain goats devil in the shortwave", "mountain goats black pear tree", "mountain goats satanic messiah", "mountain goats moon colony bloodbath", "mountain goats taboo cassette", "mountain goats come come to the sunset tree lp", "mountain goats on juhu beach", "mountain goats life of the world to come dvd", "mountain goats songs for pierre chuvin cassette", "mountain goats jam eater blues", "mountain goats all survivors pack", "mountain goats steal smoked fish"]
 
-# TODO loop over list of releases to search for, update JSON / remove things no longer available 
-
-# make sure account for plain black pressings of BPT and MCB
-releaselist = ["mountain goats zopilote machine lp", "mountain goats sweden lp", "mountain goats nothing for juice lp", "mountain goats full force galesburg lp", "mountain goats coroner's gambit lp", "mountain goats new asian cinema", "mountain goats isopanisad radio hour", "mountain goats devil in the shortwave", "mountain goats black pear tree", "mountain goats satanic messiah", "mountain goats moon colony bloodbath", "mountain goats taboo cassette", "mountain goats come come to the sunset tree", "mountain goats on juhu beach", "mountain goats black pear tree yellow", "mountain goats moon colony bloodbath green", "beatles please please me parlophone pmc 1202 first"]
-
-discogsurls = ['https://api.discogs.com/releases/1601354', 'https://api.discogs.com/releases/1191772', 'https://api.discogs.com/releases/744810', 'https://api.discogs.com/releases/523351', 'https://api.discogs.com/releases/1217804', 'https://api.discogs.com/releases/1625208', 'https://api.discogs.com/releases/1625203', 'https://api.discogs.com/releases/1210894', 'https://api.discogs.com/releases/1526609', 'https://api.discogs.com/releases/1516102','https://api.discogs.com/releases/1763341', 'https://api.discogs.com/releases/2767923', 'https://api.discogs.com/releases/769960', 'https://api.discogs.com/releases/2873969', 'https://api.discogs.com/releases/4880289', 'https://api.discogs.com/releases/5118937', 'https://api.discogs.com/releases/1350468']
+discogsurls = ['https://api.discogs.com/releases/1601354', 'https://api.discogs.com/releases/1191772', 'https://api.discogs.com/releases/744810', 'https://api.discogs.com/releases/523351', 'https://api.discogs.com/releases/1217804', 'https://api.discogs.com/releases/1625208', 'https://api.discogs.com/releases/1625203', 'https://api.discogs.com/releases/1210894', 'special', 'https://api.discogs.com/releases/1516102','special', 'https://api.discogs.com/releases/2767923', 'https://api.discogs.com/releases/769960', 'https://api.discogs.com/releases/2873969', 'https://api.discogs.com/releases/2732093', 'special', 'https://api.discogs.com/releases/2306617', 'https://api.discogs.com/releases/2797105', 'https://api.discogs.com/releases/3916742']
 
 ebaydata = {}
 discogsdata= {}
@@ -46,11 +44,35 @@ def ebayget(release):
     return get_detail_data(get_page(url))
 
 def discogsget(release):
-    total = { 'price' : '', 'url' : ''}
-    r = requests.get(release).json()
-    price1 = r['lowest_price']
-    total['price'] = price1
-    total['url'] = 'http://www.discogs.com/sell/release/' + str(r['id'])
+    if release == 'special':
+        total = { 'price' : '', 'url' : ''}    
+    else:
+        total = { 'price' : '', 'url' : ''}
+        r = requests.get(release).json()
+        price1 = r['lowest_price']
+        total['price'] = price1
+        total['url'] = 'http://www.discogs.com/sell/release/' + str(r['id'])
+        return total
+
+high = 1000000
+
+def dcompare(master):
+    total = { 'price' : high, 'url' : ''}
+    emptyurl = ''
+    i = 0
+    while i < len(master):
+        temp = discogsget(master[i])
+        emptyurl = temp['url']
+        if temp['price'] is not None and float(temp['price']) < float(total['price']):
+            total['price'] = temp['price']
+            total['url'] = temp['url']
+        else:
+            pass
+        i += 1
+    if total['price'] == high:
+        total['price'] = None
+        total['url'] = emptyurl
+    #print(total)
     return total
 
 i = 0
@@ -63,16 +85,27 @@ while j < len(discogsurls):
     discogsdata[j] = discogsget(discogsurls[j])
     j += 1
 
+discogsdata[8] = dcompare(['https://api.discogs.com/releases/1526609', 'https://api.discogs.com/releases/4880289'])
+
+discogsdata[10] = dcompare(['https://api.discogs.com/releases/1763341', 'https://api.discogs.com/releases/5118937'])
+
+discogsdata[15] = dcompare(['https://api.discogs.com/releases/15516873', 'https://api.discogs.com/releases/15517693', 'https://api.discogs.com/releases/15517068', 'https://api.discogs.com/releases/15506961'])
+
 k = 0
 while k < len(ebaydata):
-    if ebaydata[k] is not None and float(ebaydata[k]['price']) > discogsdata[k]['price']:
-        ebaydata[k] = discogsdata[k]
+    if ebaydata[k] is not None and discogsdata[k]['price'] is not None:
+        if float(ebaydata[k]['price']) > discogsdata[k]['price']:
+            ebaydata[k] = discogsdata[k]
     elif ebaydata[k] is None:
         ebaydata[k] = discogsdata[k]
     else:
         pass
     k += 1
-    
 
-print(ebaydata)
-# print(discogsdata)
+ebaydata[19] = time.time()
+
+json_object = json.dumps(ebaydata, indent = 4)
+
+with open('data.json', 'w') as outfile:
+    outfile.seek(0)
+    outfile.write(json_object)
